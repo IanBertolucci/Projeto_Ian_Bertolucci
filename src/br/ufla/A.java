e79536a9e72b770e702beaf9f2ecf64ee8a33619
IanBertolucci <ian.diasbertolucci@gmail.com>
2020-06-05 08:59:51 -0300

    Criação de classes inciais (A e B)

---
+++ src/br/ufla/A.java
@@ -0,0 +1,32 @@
+package br.ufla;
+
+public class A {
+    private int A1;
+    private float A2;
+
+    public int getA1() {
+        return A1;
+    }
+
+    public void setA1(int a1) {
+        A1 = a1;
+    }
+
+    public float getA2() {
+        return A2;
+    }
+
+    public void setA2(float a2) {
+        A2 = a2;
+    }
+
+    public void MA1(){
+        String nome = new Object(){}.getClass().getEnclosingMethod().getName();
+        System.out.println(nome);
+    }
+
+    public void MA2(){
+        String nome = new Object(){}.getClass().getEnclosingMethod().getName();
+        System.out.println(nome);
+    }
+}
---
+++ src/br/ufla/Main.java
@@ -0,0 +1,13 @@
+package br.ufla;
+
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+
+        a.MA1();
+        a.MA2();
+        b.MB1();
+        b.MB2();
+    }
+}
